05b2ce9c17a4efd29ab4f2d8ea9977635acbaa8c
Siyuan <siyuanch@kth.se>
2022-12-13 11:03:16 +0100

    Verified Final Report

--- Final_project/proj1/VAE_function.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras import backend as K
-import numpy as np
-def sampling(args):
-    z_mean = args[0]
-    z_log_sigma = args[1]
-    # z_mean, z_log_sigma = args
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], 5),
-                              mean=0., stddev=1.0)
-    return z_mean + K.exp(z_log_sigma) * epsilon
